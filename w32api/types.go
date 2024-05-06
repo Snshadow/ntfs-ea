08ace46e43fbe133cfe47d69c68dacd8fe98ba7b
Snshadow <randy0007@naver.com>
2024-05-06 21:03:07 +0900

    comment

--- w32api/types.go
+++ w32api/types.go
@@ -4,9 +4,7 @@
 package w32api
 
 const (
-	FILE_NEED_EA = 0x80
-
-	anySize = 1
+	FILE_NEED_EA = 0x80 // file should be interpreted with Extended Attributes(EA)
 )
 
 // 4 bytes aligned
@@ -15,13 +13,13 @@ type FILE_FULL_EA_INFORMATION struct {
 	Flags           uint8
 	EaNameLength    uint8
 	EaValueLength   uint16
-	EaName          [anySize]int8
-	EaValue         [anySize]byte
+	EaName          []int8 // 1 byte for ASCII character, 2 bytes for non-ASCII character
+	EaValue         []byte
 }
 
 // 4 bytes aligned
 type FILE_GET_EA_INFORMATION struct {
 	NextEntryOffset uint32
 	EaNameLength    uint8
-	EaName          [anySize]int8
+	EaName          []int8
 }
